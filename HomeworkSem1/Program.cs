06a9980cf98ca8f11932e7c1b92179f756c7ff36
prokhorov_aleksey <prohor_ad@mail.ru>
2022-06-09 07:27:17 +0300

    HomeworkCompleted

---
+++ HomeworkSem1/Program.cs
@@ -0,0 +1,79 @@
+﻿/*int num1, num2;
+
+ Console.WriteLine ("Input first integer num ");
+ num1 = Convert.ToInt32(Console.ReadLine ());
+ Console.WriteLine ("Input second integer num ");
+ num2 = Convert.ToInt32(Console.ReadLine ());
+
+if (num1 > num2)
+{
+    Console.WriteLine("Max: " + num1 );
+    Console.Write("Min: " + num2);
+}
+else
+{
+    Console.WriteLine("Max: " + num2);
+    Console.Write("Min: " + num1);
+}
+*/
+
+
+/*int num1, num2, num3,max;
+
+ Console.Write ("Input first integer num ");
+ num1 = Convert.ToInt32(Console.ReadLine ());
+ Console.Write ("Input second integer num ");
+ num2 = Convert.ToInt32(Console.ReadLine ());
+ Console.Write ("Input third integer num ");
+ num3 = Convert.ToInt32(Console.ReadLine ());
+
+ if(num1 > num2)
+ {
+    max = num1;
+ }
+ else
+ {
+      max = num2;
+ }
+ if(num3 > max)
+ {
+     max = num3;
+ }
+ Console.Write("Max: " + max);
+ */
+
+ /*int num;
+ Console.Write ("Input num ");
+ num = Convert.ToInt32(Console.ReadLine ());
+
+if(num % 2 == 0)
+{
+    Console.Write("Четное!");
+}
+else
+{
+    Console.Write("Нечетное!");
+}
+*/
+
+/*int N, current = 2;
+Console.Write("Input N: ");
+N = Convert.ToInt32(Console.ReadLine ());
+
+if(N % 2 == 0)
+{
+    while(current <= N)
+    {
+    Console.Write(current + " ");
+    current = current + 2;
+    }
+}
+else
+{
+    while(current < N)
+    {
+    Console.Write(current + " ");
+    current = current + 2;
+    }
+}
+*/
